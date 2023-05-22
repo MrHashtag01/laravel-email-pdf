876089651e713d96f358c58859e23e94d45e5ff4
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-05-22 17:20:19 +0400

    Defined routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PDFController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('send-email-pdf', [PDFController::class, 'index']);
